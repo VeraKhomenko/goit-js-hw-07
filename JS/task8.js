53552b0262efdc5df1177b022163382208102c04
VeraKhomenko <pochtaVKh@gmail.com>
2020-12-20 22:34:18 +0200

    render list in one operation
    removed comments

--- JS/task8.js
+++ JS/task8.js
@@ -1,63 +1,39 @@
-/*Задание 8 - дополнительное, выполнять не обязательно
-Напиши скрипт создания 
-и очистки коллекции элементов.
- Пользователь вводит количество элементов в input и нажимает кнопку Создать,
-  после чего рендерится коллекция.
-   При нажатии на кнопку Очистить, коллекция элементов очищается.
-
-Создай функцию createBoxes(amount), которая принимает 1 параметр amount - число.
- Функция создает столько div, сколько указано в amount и добавляет их в div#boxes.
-
-Каждый созданный div:
-
-// Имеет случайный rgb цвет фона
-// Размеры самого первого div - 30px на 30px
-Каждый следующий div после первого, должен быть шире и выше предыдущего на 10px
-Создай функцию destroyBoxes(), которая очищает div#boxes.
-
-<div id="controls">
-          <input type="number" min="0" max="100" step="1" />
-          <button type="button" data-action="render">Создать</button>
-          <button type="button" data-action="destroy">Очистить</button>
-        </div>
-        <div id="boxes"></div>
-*/
-
 let boxSize = 30;
 const mainDiv = document.querySelector('div#controls');
 
 const renderBtn = document.querySelector('[data-action="render"]');
 const destroyBtn = document.querySelector('[data-action="destroy"]');
+const inputValue = document.querySelector('div#controls input');
+const divBoxes = document.querySelector('div#boxes');
 
 renderBtn.addEventListener('click', onClickRender);
 destroyBtn.addEventListener('click', onClickDestroy);
 
 function onClickRender() {
-  const inputValue = document.querySelector('div#controls input').value;
-  createBoxes(inputValue);
+  createBoxes(inputValue.value);
 }
 function onClickDestroy() {
-  const divDestroy = document.querySelector('div#boxes');
-  divDestroy.innerHTML = '';
-  // for (let keyNode of divDestroy) keyNode.remove();
+  divBoxes.innerHTML = '';
 }
 
 function createBoxes(amount) {
-  const divBoxes = document.querySelector('div#boxes');
   const step = 10;
+  const arr = [];
   for (let index = 0; index < amount; index++) {
-    divBoxes.insertAdjacentHTML(
-      'beforeEnd',
+    arr.push(
       `<div style="width:${boxSize + index * step}px; height:${
         boxSize + index * step
       }px; background-color: ${randColor()};"></div>`,
     );
   }
+  arr.map(box => divBoxes.insertAdjacentHTML('beforeend', box));
 }
 
 function randColor() {
-  let r = Math.floor(Math.random() * 256),
-    g = Math.floor(Math.random() * 256),
-    b = Math.floor(Math.random() * 256);
-  return '#' + r.toString(16) + g.toString(16) + b.toString(16);
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
